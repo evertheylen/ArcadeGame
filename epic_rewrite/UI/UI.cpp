9a0fca877ef0f6d0e27df31ecbe5289c26d34f12
Stijn Janssens <janssens.stijn@hotmail.com>
2015-04-17 11:14:25 +0200

    Basic UI Finished??

--- epic_rewrite/UI/UI.cpp
+++ epic_rewrite/UI/UI.cpp
@@ -7,22 +7,23 @@
 
 #include "../UI/UI.h"
 //#include "../../src/game/parser/game_parser.h"	TODO FIX THE PARSERS!
-#include <string>
+#include <fstream>
 
 UI::UI() {
 	std::cout << "\n\nWelcome to the Wonderful ARCADE GAME of Evert and Stijn!\n\n\nThis interactive User Interface is able to do a few simple things:\n\n";
 	std::cout << "  *  Read an xml-file for the board\t\t[ type read_board <board_file> ]\n\n";
 	std::cout << "  *  Read an xml-file for the actions\t\t[ type read_actions <actions_file> ]\n\n";
 	std::cout << "  *  Simulate one or more of the actions\t[ type simulate <amount of moves> ]\tdefault=1\n\n";
-	std::cout << "  *  Write the current board to a file\t\t[ type write_board <output_file> ]\n\n";
-	std::cout << "  *  Write the rest of the actions to a file\t[ type write_actions <output_file> ]\n\n";
+	std::cout << "  *  Write the current board to a file\t\t[ type write_board <output_file> ]\tdefault=HuidigSpeelveld.txt\n\n";
+	std::cout << "  *  Write the rest of the actions to a file\t[ type write_actions <output_file> ]\tdefault=ResterendeBewegingen.txt\n\n";
 	std::cout << "  *  Print out the simple graphical impression\t[ type show <output_stream> ]\t\tdefault=std::cout\n\n";
 	std::cout << "\n\tIf you ever want to see this documentation again please type --help.\n\n";
 
 	bool win = false;
 	bool board_loaded = false;
 	bool moves_loaded = false;
-	bool no_game = true;
+	bool parsed = false;
+	Game g;
 	//TiXmlDocument doc_board, doc_moves;
 	while (win == false) {
 		std::string input;
@@ -41,9 +42,16 @@ UI::UI() {
 			}
 			board_file = input.substr(compare_str.size()+1, input.size()).c_str();
 			//board_loaded = doc_board.LoadFile(board_file);
-
+			std::cout << "Reading board, automatically setting board_loaded = true\n";
+			board_loaded = true;
 			if (!board_loaded) {
 				std::cerr << "Error loading board.\n";
+			}
+			if (board_loaded && moves_loaded && !parsed) {
+				//Game_parser gp(&std::cerr, std::string(board_file));
+				//Game g = *gp.parse_game(doc_board.FirstChildElement(), doc_moves.FirstChildElement());
+				std::cout << "Parsing and initializing game!\n";
+				parsed = true;
 			}
 			continue;
 		}
@@ -56,18 +64,25 @@ UI::UI() {
 			}
 			const char* file = input.substr(compare_str.size()+1, input.size()).c_str();
 			//moves_loaded = doc_moves.LoadFile(file);
-
+			std::cout << "Reading moves, automatically setting moves_loaded = true\n";
+			moves_loaded = true;
 			if (!moves_loaded) {
 				std::cerr << "Error loading moves.\n";
+			}
+			if (board_loaded && moves_loaded && !parsed) {
+				//Game_parser gp(&std::cerr, std::string(board_file));
+				//Game g = *gp.parse_game(doc_board.FirstChildElement(), doc_moves.FirstChildElement());
+				std::cout << "Parsing and initializing game!\n";
+				parsed = true;
 			}
 			continue;
 		}
 
-		if (board_loaded && moves_loaded && no_game) {
+		if (board_loaded && moves_loaded && !parsed) {
 			//Game_parser gp(&std::cerr, std::string(board_file));
 			//Game g = *gp.parse_game(doc_board.FirstChildElement(), doc_moves.FirstChildElement());
 			std::cout << "Parsing and initializing game!\n";
-			no_game = false;
+			parsed = true;
 		}
 
 
@@ -80,13 +95,89 @@ UI::UI() {
 				amount = atoi(input.substr(compare_str.size()+1, input.size()).c_str());
 				if (amount == 0) amount = 1;
 			}
-			for (int i = 0; i < amount; i++) {
-				//g.do_action;
+			if (parsed) {
+				do_action(amount);
+				continue;
+			} else {
+				std::cout << "Game is not initialized or parsed!\n";
+				continue;
 			}
+		}
+
+		compare_str = "write_board";
+		if (!input.compare(0, compare_str.size(), compare_str)) {
+			if (!parsed) {
+				std::cout << "Game is not initialized or parsed!\n";
+				continue;
+			}
+			if (input.size() <= compare_str.size()+1) {
+				write_board("HuidigSpeelveld.txt", g);
+				continue;
+			}
+			std::string file = input.substr(compare_str.size()+1, input.size()).c_str();
+			write_board(file, g);
 			continue;
 		}
 
+		compare_str = "write_actions";
+		if (!input.compare(0, compare_str.size(), compare_str)) {
+			if (!parsed) {
+				std::cout << "Game is not initialized or parsed!\n";
+				continue;
+			}
+			if (input.size() <= compare_str.size()+1) {
+				write_board("ResterendeBewegingen.txt", g);
+				continue;
+			}
+			std::string file = input.substr(compare_str.size()+1, input.size()).c_str();
+			write_actions(file, g);
+			continue;
+		}
 
+		compare_str = "show";
+		if (!input.compare(0, compare_str.size(), compare_str)) {
+			if (!parsed) {
+				std::cout << "Game is not initialized or parsed!\n";
+				continue;
+			}
+			if (input.size() <= compare_str.size()+1) {
+				show(std::cout);
+				continue;
+			}
+			std::ofstream stream;
+			stream.open(input.substr(compare_str.size()+1, input.size()).c_str());
+			show(stream);
+			stream.close();
+			continue;
+		}
+	}
+}
+
+void UI::show(std::ostream& out) {
+	out << "Showing the board!\n";
+	//out << g << std::endl;
+}
+
+void UI::write_board(std::string file, Game& g) {
+	std::ofstream output_file;
+	output_file.open(file.c_str());
+	output_file << "Hier verschijnt later het huidige speelveld!\n";
+	//g.writeBoard(output_file);
+	output_file.close();
+}
+
+void UI::write_actions(std::string file, Game& g) {
+	std::ofstream output_file;
+	output_file.open(file.c_str());
+	output_file << "Hier verschijnen later de resterende bewegingen!\n";
+	//g.writeActions(output_file);
+	output_file.close();
+}
+
+void UI::do_action(int amount) {
+	for (int i = 0; i < amount; i++) {
+		std::cout << "Doing an action\n";
+		//g.do_action;
 	}
 }
 
@@ -95,8 +186,8 @@ void UI::help() {
 	std::cout << "  *  Read an xml-file for the board\t\t[ type read_board <board_file> ]\n\n";
 	std::cout << "  *  Read an xml-file for the actions\t\t[ type read_actions <actions_file> ]\n\n";
 	std::cout << "  *  Simulate one or more of the actions\t[ type simulate <amount of moves> ]\tdefault=1\n\n";
-	std::cout << "  *  Write the current board to a file\t\t[ type write_board <output_file> ]\n\n";
-	std::cout << "  *  Write the rest of the actions to a file\t[ type write_actions <output_file> ]\n\n";
+	std::cout << "  *  Write the current board to a file\t\t[ type write_board <output_file> ]\tdefault=HuidigSpeelveld.txt\n\n";
+	std::cout << "  *  Write the rest of the actions to a file\t[ type write_actions <output_file> ]\tdefault=ResterendeBewegingen.txt\n\n";
 	std::cout << "  *  Print out the simple graphical impression\t[ type show <output_stream> ]\t\tdefault=std::cout\n\n";
 	std::cout << "\n\tIf you ever want to see this documentation again please type --help.\n\n";
 }
--- epic_rewrite/UI/UI.h
+++ epic_rewrite/UI/UI.h
@@ -6,7 +6,7 @@
  */
 
 #include "../../lib/tinyxml/tinyxml.h"
-#include <fstream>
+#include <string>
 #include <iostream>
 #include "../game/game.h"
 
@@ -18,15 +18,15 @@ public:
 
 	UI();
 
-	void input_board(TiXmlDocument& board_doc);
-	void input_actions(TiXmlDocument& action_doc);
+	//void input_board(TiXmlDocument& board_doc); Overbodig, wordt enkel gebruikt vanuit constructor.
+	//void input_actions(TiXmlDocument& action_doc);
 
 	void do_action(int amount = 1);
 
-	void write_board(std::fstream& out);
-	void write_actions(std::fstream& out);
+	void write_board(std::string file, Game& g);
+	void write_actions(std::string file, Game& g);
 
-	void show(std::ostream& out = std::cout);
+	void show(std::ostream& out);
 
 	void help();
 
