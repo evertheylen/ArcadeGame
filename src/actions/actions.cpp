0e64fc02ecfcc13dae4e4345eee0dd5b9149dba9
Stijn Janssens <janssens.stijn@hotmail.com>
2015-05-04 20:45:00 +0200

    weird shit with enum copy constructors

--- src/actions/actions.cpp
+++ src/actions/actions.cpp
@@ -4,8 +4,16 @@
 
 // Direction
 
+bool Direction::properlyInitialized() {
+	return (initCheck == this);
+}
+
+
 Direction::Direction(Direction::Dirk d):
-		dir(d) {}
+		dir(d) {
+	initCheck = this;
+	ENSURE(properlyInitialized(), "Copy constructor must end...");
+}
 
 Direction::Direction(std::string& s) {
 	if (s == "OMHOOG") {
@@ -20,10 +28,25 @@ Direction::Direction(std::string& s) {
 		//std::cerr << "Did not recognize type " << dir_s << " as a direction.";
 		dir = Dirk::no_dir;
 	}
+	initCheck = this;
+	ENSURE(properlyInitialized(), "Constructor must end...");
 }
 
+Direction::Direction(const Direction& that) {
+	dir = that.dir;
+	initCheck = this;
+	ENSURE(properlyInitialized(), "Copy constructor must end...");
+}
+
+Direction& Direction::operator=(const Direction& that) {
+	dir = that.dir;
+	initCheck = this;
+	ENSURE(properlyInitialized(), "Copy by assingment must end...");
+	return *this;
+}
 
 void Direction::move_to(unsigned int& x, unsigned int& y) {
+	REQUIRE(properlyInitialized(), "Direction wasn't initialized when calling move_to.");
 	switch (dir) {
 		case Dirk::up:
 			y++;
@@ -42,6 +65,7 @@ void Direction::move_to(unsigned int& x, unsigned int& y) {
 
 
 void Direction::move_from(unsigned int& x, unsigned int& y) {
+	REQUIRE(properlyInitialized(), "Direction wasn't initialized when calling move_from.");
 	switch (dir) {
 		case Dirk::up:
 			y--;
@@ -60,6 +84,7 @@ void Direction::move_from(unsigned int& x, unsigned int& y) {
 
 
 std::ostream& operator<<(std::ostream& out, Direction& d) {
+	REQUIRE(d.properlyInitialized(), "Direction wasn't initialized when calling operator <<.");
 	switch (d.dir) {
 		case Direction::Dirk::up:
 			out << "OMHOOG";
@@ -79,19 +104,43 @@ std::ostream& operator<<(std::ostream& out, Direction& d) {
 }
 
 Direction::Dirk Direction::get_dir() {
+	REQUIRE(properlyInitialized(), "Direction wasn't initialized when calling get_dir.");
 	return dir;
 }
 
 // Action
 
 Action::Action(Actor* _actor, std::string& dirname):
-		actor(_actor), dir(dirname) {}
+		actor(_actor), dir(dirname) {
+	initCheck = this;
+}
+
+bool Action::properlyInitialized() {
+	return (initCheck == this);
+}
+
+Action::Action(const Action& that) {
+	//dir(that.dir);	 TODO COPY CONSTRUCTOR FOR DIR FIXEN
+	actor = that.actor;
+	initCheck = this;
+	ENSURE(properlyInitialized(), "Copy constructor must end...");
+}
+
+Action& Action::operator=(const Action& that) {
+	//dir();
+	actor = that.actor;
+	initCheck = this;
+	ENSURE(properlyInitialized(), "Copy by assingment must end...");
+	return *this;
+}
 
 Actor* Action::get_actor() {
+	REQUIRE(properlyInitialized(), "Action wasn't initialized when calling get_actor.");
 	return actor;
 }
 
 Direction Action::get_dir() {
+	REQUIRE(properlyInitialized(), "Action wasn't initialized when calling get_dir.");
 	return dir;
 }
 
--- src/actions/actions.h
+++ src/actions/actions.h
@@ -16,18 +16,53 @@ class Direction {
 public:
 	enum class Dirk {up, left, down, right, no_dir};
 	
+	/**
+	 * ENSURE(properlyInitialized(), "Constructor must end...")
+	 */
 	Direction(std::string& s);
+
+	/**
+	 * ENSURE(properlyInitialized(), "Constructor must end...")
+	 */
 	Direction(Dirk d);
 	
+	/** \brief Copy constructor
+	 *
+	 * ENSURE(properlyInitialized(), "Copy constructor must end...")
+	 */
+	Direction(const Direction& that);
+
+	/** \brief Copy by assignment
+	 *
+	 * ENSURE(properlyInitialized(), "Copy by assignment must end...")
+	 */
+	Direction& operator=(const Direction& that);
+
+	/**
+	 * REQUIRE(d.properlyInitialized(), "Direction wasn't initialized when calling operator <<.")
+	 */
 	friend std::ostream& operator<<(std::ostream& out, Direction& d);
 	
+	/**
+	 * REQUIRE(properlyInitialized(), "Direction wasn't initialized when calling get_dir.")
+	 */
 	Direction::Dirk get_dir();
 
+	/**
+	 * REQUIRE(properlyInitialized(), "Direction wasn't initialized when calling move_to.")
+	 */
 	void move_to(unsigned int& x, unsigned int& y);
+
+	/**
+	 * REQUIRE(properlyInitialized(), "Direction wasn't initialized when calling move_from.")
+	 */
 	void move_from(unsigned int& x, unsigned int& y);
 	
+	bool properlyInitialized();
+
 private:
 	Dirk dir;
+	Direction* initCheck;
 };
 
 
@@ -37,13 +72,39 @@ protected:
 	Direction dir;
 	
 public:
+	/**
+	 * ENSURE(properlyInitialized(), "Constructor must end...")
+	 */
 	Action(Actor* _actor, std::string& dirname);
 	
+	/** \brief Copy constructor
+	 *
+	 * ENSURE(properlyInitialized(), "Copy constructor must end...")
+	 */
+	Action(const Action& that);
+
+	/** \brief Copy by assignment
+	 *
+	 * ENSURE(properlyInitialized(), "Copy by assignment must end...")
+	 */
+	Action& operator=(const Action& that);
+
+	/**
+	 * REQUIRE(properlyInitialized(), "Action wasn't initialized when calling get_actor.")
+	 */
 	Actor* get_actor();
 
+	/**
+	 * REQUIRE(properlyInitialized(), "Action wasn't initialized when calling get_dir.")
+	 */
 	Direction get_dir();
 
 	virtual bool execute(Game* g) = 0;
+
+	bool properlyInitialized();
+
+private:
+	Action* initCheck;
 };
 
 
